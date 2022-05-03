e293988a6e0243eb05702670f7f1271b25537035
Balbaar <hjalmar.bulow@gmail.com>
2022-05-03 14:56:58 +0200

    fixed issue with only opening one page

--- main.py
+++ main.py
@@ -14,14 +14,16 @@ math_urls = {1: "https://konto.nok.se/login/nokflex",
 physics_urls = {1: "https://www.liber.se/plus/E471262201.pdf"}
 
 #Swedish
-swedish_urls = {1: "https://classroom.google.com/u/3/c/Mzc4MDUzNjM1MTA0"}
+swedish_urls = {1: "https://classroom.google.com/u/3/c/Mzc4MDUzNjM1MTA0",
+                2: "https://drive.google.com/drive/u/3/my-drive"}
 
 #English
 english_urls = {1: "https://classroom.google.com/u/3/c/Mzc4MDQwMDA0Nzg3"}
 
 #History
 history_urls = {1: "https://classroom.google.com/u/3/c/NDMyMTgwMjU5Nzk3",
-                2: "https://docs.google.com/document/d/12u8Sm0bqaV7q0vPWeca3Jch3qPrCp3b2iGoonqRL2Vs/edit"}
+                2: "https://docs.google.com/document/d/12u8Sm0bqaV7q0vPWeca3Jch3qPrCp3b2iGoonqRL2Vs/edit",
+                3: "https://drive.google.com/drive/u/3/my-drive"}
 
 #Technic
 
@@ -36,15 +38,16 @@ def clicked(icon, item):
             webbrowser.get(google_location).open(physics_urls[page])
 
     elif str(item) == "Svenska":
-        for page in physics_urls:
+        for page in swedish_urls:
             webbrowser.get(google_location).open(swedish_urls[page])
 
     elif str(item) == "Engelska":
-        for page in physics_urls:
+        for page in english_urls:
             webbrowser.get(google_location).open(english_urls[page])
 
     elif str(item) == "Historia":
-        for page in physics_urls:
+        for page in history_urls:
+            print(page)
             webbrowser.get(google_location).open(history_urls[page])
 
     elif str(item) == "Stäng":
